221f4494f4bebc57894ec0ff3d392bd2762f5d49
onlysilvercrow <theonlyplatinumking@gmail.com>
2023-04-04 17:07:55 +0100

    missing and unauthorised page css changes

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,8 +1,15 @@
+import { useMediaQuery } from "react-responsive"
+
 const Missing = () => {
+    const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1101px)'})
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1100px)' })
     return(
-        <section>
-            <p>Page Not Found</p>
-        </section>
+        <div style={{display:"flex", alignItems:"center", justifyContent:"center", width:"100%"}}>
+            {isDesktopOrLaptop && <p style = {{ fontFamily:'F1Font', fontSize: '6em', textAlign: "center"}}>Page Not Found</p>}
+    
+            {isTabletOrMobile && <p style = {{ fontFamily:'F1Font', fontSize: '3em', textAlign: "center"}}>Page Not Found</p>}
+    
+        </div>
     )
 }
 export default Missing
--- src/components/Unauthorised.js
+++ src/components/Unauthorised.js
@@ -3,19 +3,20 @@ import { useMediaQuery } from "react-responsive"
 const Unauthorised = () => {
     const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1101px)'})
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1100px)' })
+    
     return(
-        <>
-       {isDesktopOrLaptop && <div style = {{display:"flex", paddingTop:"14em", flexDirection:"column"}}>
-            <p style = {{ fontFamily:'F1Font', fontSize: '6em'}}>Unauthorised Access</p>
-            <p style = {{fontFamily:'F1Font', fontSize: '4em'}}>Return to Home page</p>
+        <div style={{display:"flex", alignItems:"center", justifyContent:"center", width:"100%"}}>
+       {isDesktopOrLaptop && <div style = {{display:"flex", flexDirection:"column"}}>
+            <p style = {{ fontFamily:'F1Font', fontSize: '6em', textAlign: "center"}}>Unauthorised Access</p>
+            <p style = {{fontFamily:'F1Font', fontSize: '4em', textAlign: "center"}}>Return to Home page</p>
         </div>}
 
-        {isTabletOrMobile && <div style = {{display:"flex", paddingTop:"14em", flexDirection:"column"}}>
-            <p style = {{ fontFamily:'F1Font', fontSize: '3em'}}>Unauthorised Access</p>
-            <p style = {{fontFamily:'F1Font', fontSize: '2em'}}>Return to Home page</p>
+        {isTabletOrMobile && <div style = {{display:"flex", flexDirection:"column"}}>
+            <p style = {{ fontFamily:'F1Font', fontSize: '3em', textAlign: "center"}}>Unauthorised Access</p>
+            <p style = {{fontFamily:'F1Font', fontSize: '2em', textAlign: "center"}}>Return to Home page</p>
         </div>}
 
-        </>
+        </div>
     )
 }
 export default Unauthorised
