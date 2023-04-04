653e81f51fe5f806596d6f3eba4bd86272731692
onlysilvercrow <theonlyplatinumking@gmail.com>
2023-04-04 16:31:39 +0100

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ const Footer = () => {
         <div className> 
           <ul style={{"listStyleType":"none"}}>
             <li><b>Creator:</b> Sahil Patel</li>
-            <li><b>Credit:</b> Mantra Kusumgar & Deep Harquissandas</li>
             <li><b>Contact:</b> sahilpatel1906@gmail.com</li>
             <li><b>Note:</b> API data obtained from <a href = "https://ergast.com/mrd/" style = {{color: "white"}}>https://ergast.com/mrd/</a></li>            
           </ul>
