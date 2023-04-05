f7bbf73bbe6d474a06f2840f70a2a8264828fbb7
onlysilvercrow <theonlyplatinumking@gmail.com>
2023-04-05 01:28:16 +0100

    register success page alteration

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,8 +10,8 @@ const REGISTER_URL = 'register'
 
 
 const Register = () => {
+    const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1101px)'})
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1100px)' })
-
     const userRef = useRef()
     const errRef = useRef()
 
@@ -88,12 +88,17 @@ const Register = () => {
     return(
         <>
         {success ? (
-            <section>
-                <h1>Success!</h1>
-                <p>
-                    <a href="https://ambitious-pebble-02008e903.2.azurestaticapps.net/login" className = "login-register-links">Sign In</a>
-                </p>
-            </section>
+            <div style={{display:"flex", alignItems:"center", justifyContent:"center", width:"100%"}}>
+                 {isDesktopOrLaptop && <div style = {{display:"flex", flexDirection:"column"}}>
+                    <p style = {{ fontFamily:'F1Font', fontSize: '6em', textAlign: "center"}}>Success!</p>
+                    <p style = {{fontFamily:'F1Font', fontSize: '4em', textAlign: "center"}}>Please Login</p>
+                </div>}
+
+                {isTabletOrMobile && <div style = {{display:"flex", flexDirection:"column"}}>
+                    <p style = {{ fontFamily:'F1Font', fontSize: '3em', textAlign: "center"}}>Success!</p>
+                    <p style = {{fontFamily:'F1Font', fontSize: '2em', textAlign: "center"}}>Please Login</p>
+                </div>}
+            </div>
         ) : (      
         <section className="acc-form">
             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
